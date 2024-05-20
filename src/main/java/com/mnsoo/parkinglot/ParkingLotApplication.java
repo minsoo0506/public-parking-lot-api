5716b7133d8b2ca4ed5274c60c5c1ea4d788e91b
minsoo0506 <mspark010506@gmail.com>
2024-05-20 16:46:43 +0900

    feat: s3 연결

--- src/main/java/com/mnsoo/parkinglot/ParkingLotApplication.java
+++ src/main/java/com/mnsoo/parkinglot/ParkingLotApplication.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // 서울시 공영 주차장 자리 확인 서비스
-@SpringBootApplication
+
 @EnableJpaAuditing
 @EnableScheduling
+@SpringBootApplication(exclude = {
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+})
 public class ParkingLotApplication {
 
 	public static void main(String[] args) {
